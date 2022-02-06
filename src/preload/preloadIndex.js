dd19cd239f2d90f7bfa55552a8b9be8d52b73c83
CamielYang <Yangcamiel@gmail.com>
2022-02-06 11:23:51 +0100

    Code cleanup
    Changes:
    - Added methods in the contextBridge to expose is for the renderer file.
    - Cleaned up the preload file and moved the methods to the correct folder (renderer js file).

--- src/preload/preloadIndex.js
+++ src/preload/preloadIndex.js
@@ -1,120 +1,14 @@
-const { contextBridge, ipcRenderer, clipboard } = require('electron');
+const { 
+    contextBridge, 
+    ipcRenderer, 
+    clipboard, 
+} = require('electron');
 const loudness = require('loudness')
 const clipboardListener = require('clipboard-event');
 clipboardListener.startListening();
 
 // Waits until the view is completely loaded
 window.addEventListener("DOMContentLoaded", () => {    
-    const modal = document.getElementById("myModal");
-    const contentDiv = document.getElementById("clipboard");
-    const tempPath = "templates/";
-
-    window.onload = () => {
-        updateClipboard();
-    }
-    
-    /* CLIPBOARD FUNTIONS */
-    // Event for clearing clipboard
-    document.getElementById("clearClipboard").addEventListener("click", function() {
-        clipboard.clear();
-    });
-    
-    // Update clipboard when clipboard change event is fired
-    clipboardListener.on('change', () => {
-        updateClipboard();
-    });
-
-    // Update cllipboard content
-    function updateClipboard() {
-        const text = clipboard.readText();
-        const image = clipboard.readImage();
-    
-        if (text) {
-            setClipboardText(text);
-        } 
-        else if (!image.isEmpty()) {
-            setClipboardImage(image);
-        } 
-        else {
-            setClipboardEmpty();
-        }
-    }
-
-    // Set the image in the clipboard content
-    function setClipboardImage(image) {
-        const imageUrl = image.toDataURL();
-        contentDiv.innerHTML = `<img class="clipboard-image" id="clipboardImg" src="${imageUrl}">`;
-        
-        const clipboardImage = document.getElementById("clipboardImg");
-        clipboardImage.onclick = function() {
-            loadImageModal(imageUrl);
-        }
-    }
-
-    // Set the text in the clipboard content
-    function setClipboardText(text) {
-        contentDiv.innerHTML = `
-        <div class="clipboard-text">
-            <p >${escapeHtml(text)}</p>
-        </div>`;
-    }
-
-    // Default empty clipboard
-    function setClipboardEmpty() {
-        contentDiv.innerHTML = `
-        <p class="clipboard-empty">
-            Clipboard is empty
-        </p>`;
-    }
-    
-    // Convert plain html to html entities to prevent xss injection and html interruption
-    function escapeHtml(text) {
-        var map = {
-          '&': '&amp;',
-          '<': '&lt;',
-          '>': '&gt;',
-          '"': '&quot;',
-          "'": '&#039;'
-        };
-        
-        return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-    }   
-    
-    /* MODAL FUNCTIONS */
-    // Show modal
-    function setModal() {
-        modal.classList.remove("hidden");
-    }
-    
-    // Hide modal
-    function unsetModal() {
-        modal.classList.add("hidden");
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            unsetModal();
-        }
-    }
-    
-    // Load modal content by fetching a template
-    async function loadModal(file) {  
-        let request = await fetch(tempPath + file);
-        let response = await request.text();
-        document.getElementById("myModal").innerHTML = response;
-        
-        setModal();
-    }
-    
-    // Load modal that previews the image
-    async function loadImageModal(image) {
-        await loadModal("imageModal.html");
-        
-        const modal = document.getElementById("modalImage");
-        modal.src = image;
-    }
-
     /* ipcRenderers */
     // Event for closing the main window on click
     document.getElementById("closeWinBtn").addEventListener("click", function() {
@@ -128,8 +22,18 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 contextBridge.exposeInMainWorld("api", {
+    // Volume control
     getVolume: () => loudness.getVolume(),
     setVolume: (value) => loudness.setVolume(value),
     getMuted: () => loudness.getMuted(),
     setMuted: (bool) => loudness.setMuted(bool),
+
+    // Clipboard
+    onClipboardChanged: (event, listener) => clipboardListener.on(event, listener),
+    clearClipboard: () => clipboard.clear(),
+    readClipboardText: () => clipboard.readText(),
+    readClipboardImage: () => clipboard.readImage().toDataURL(),
+    imageIsEmpty: () => {
+        return clipboard.readImage().isEmpty()
+    },
 })
--- src/renderer/js/index.js
+++ src/renderer/js/index.js
@@ -1,5 +1,8 @@
 const volumebar =  document.getElementById("volumebar");
 const volumeIcon = document.getElementById("volumeIcon");
+const modal = document.getElementById("myModal");
+const contentDiv = document.getElementById("clipboard");
+const tempPath = "templates/";
 let value;
 let mute;
 
@@ -7,6 +10,7 @@ window.onload = function () {
     startTime();
     getDate();
     updateAllVolume();
+    updateClipboard();
 }
 
 /* DATE AND TIME */
@@ -114,3 +118,106 @@ function updateVolumeIcon(value, mute) {
 document.getElementById("clearNotepad").addEventListener("click", function() {
     document.getElementById('notepad').value = '';
 });
+
+
+/* Clipboard */
+// Event for clearing clipboard
+document.getElementById("clearClipboard").addEventListener("click", function() {
+    api.clearClipboard();
+});
+
+// Update clipboard when clipboard change event is fired
+api.onClipboardChanged('change', () => {
+    updateClipboard();
+});
+
+// Update cllipboard content
+function updateClipboard() {
+    const text = api.readClipboardText();
+    const image = api.readClipboardImage();
+    
+    if (text) {
+        setClipboardText(text);
+    } 
+    else if (!api.imageIsEmpty()) {
+        setClipboardImage(image);
+    } 
+    else {
+        setClipboardEmpty();
+    }
+}
+
+// Set the image in the clipboard content
+function setClipboardImage(image) {
+    contentDiv.innerHTML = `<img class="clipboard-image" id="clipboardImg" src="${image}">`;
+    
+    const clipboardImage = document.getElementById("clipboardImg");
+    clipboardImage.onclick = function() {
+        loadImageModal(image);
+    }
+}
+
+// Set the text in the clipboard content
+function setClipboardText(text) {
+    contentDiv.innerHTML = `
+    <div class="clipboard-text">
+        <p >${escapeHtml(text)}</p>
+    </div>`;
+}
+
+// Default empty clipboard
+function setClipboardEmpty() {
+    contentDiv.innerHTML = `
+    <p class="clipboard-empty">
+        Clipboard is empty
+    </p>`;
+}
+
+// Convert plain html to html entities to prevent xss injection and html interruption
+function escapeHtml(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}  
+
+/* MODAL FUNCTIONS */
+// Show modal
+function setModal() {
+    modal.classList.remove("hidden");
+}
+
+// Hide modal
+function unsetModal() {
+    modal.classList.add("hidden");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        unsetModal();
+    }
+}
+
+// Load modal content by fetching a template
+async function loadModal(file) {  
+    let request = await fetch(tempPath + file);
+    let response = await request.text();
+
+    document.getElementById("myModal").innerHTML = response;
+    
+    setModal();
+}
+
+// Load modal that previews the image
+async function loadImageModal(image) {
+    await loadModal("imageModal.html");
+    
+    const modal = document.getElementById("modalImage");
+    modal.src = image;
+}
