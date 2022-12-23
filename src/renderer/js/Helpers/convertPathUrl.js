f633cada868514c158c59f1b129d45fa4b8bbcf0
CamielYang <70901975+CamielYang@users.noreply.github.com>
2022-12-23 11:26:51 +0100

    Delete src/renderer/js/Helpers directory

--- src/renderer/js/Helpers/convertPathUrl.js
+++
@@ -1,7 +0,0 @@
-// Reformat path to return usable path for css
-export function convertPathUrl(filePath) {
-    if (!filePath) {
-        return null;
-    }
-    return filePath.replaceAll("\\", "/");
-}
